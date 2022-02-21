620cc02ca218c349a5b828bab2a9b8aff07148c2
jason07289 <jason07999@naver.com>
2022-02-21 16:33:07 +0900

    SecurityConfig add

--- securityExample/src/main/java/com/example/springSecurity/service/MemberService.java
+++ securityExample/src/main/java/com/example/springSecurity/service/MemberService.java
@@ -1,5 +1,18 @@
 package com.example.springSecurity.service;
 
-public class MemberService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class MemberService implements UserDetailsService{@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
